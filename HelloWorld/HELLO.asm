e01c1c22830fb82773be73c25e78d04fb2303c93
izemskov <pascal.ilya@gmail.com>
2021-08-26 19:05:24 +0300

    Save and restore old stack and registry values

--- HelloWorld/HELLO.asm
+++ HelloWorld/HELLO.asm
@@ -9,9 +9,16 @@
 ;AUTHOR:  Ilya Zemskov, 2021
 ;         pascal.ilya@gmail.com
 
-        ORG 60000
+        ORG #6000   ; start address
+        ; save old stack value
+        LD (ALASM_STACK),SP
+        LD SP,(PROGRAM_STACK)
+        ;save old registry values
+        PUSH AF
+        PUSH BC
+        PUSH DE
 
-        LD A,2
+        LD A,2      ; set output stream to screen
         CALL 5633
 
         LD A,22
@@ -24,4 +31,14 @@
         LD A,"X"
         RST 16
 
+        ; return old registry values
+        POP DE
+        POP BC
+        POP AF
+        ; return old stack value
+        LD SP,(ALASM_STACK)
         RET
+
+; GLOBAL VARIABLES AND DATA
+PROGRAM_STACK   DEFW #6000
+ALASM_STACK     DEFW #0000
