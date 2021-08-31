d0a16504d95f41be94e7ac3f5f09a7ecd8e1054d
izemskov <pascal.ilya@gmail.com>
2021-08-31 15:44:08 +0300

    Set screen attribute

---
+++ SetScreenAttribute/SCRATR.asm
@@ -0,0 +1,96 @@
+;SET SCREEN ATTRIBUTE
+;
+;ASM:     ALASM v5.07
+;CPU:     Zilog Z80, 3.5MHz
+;RAM:     48Kb or 128Kb
+;SCREEN:  265x192 pixels,
+;         32x24 color attributes
+;CTRL:    Keyboard and joystick
+;
+;AUTHOR:  Ilya Zemskov, 2021
+;         pascal.ilya@gmail.com
+
+SCREEN_STREAM    EQU 5633
+PRINT_STRING     EQU 8252
+
+        ORG #6000   ; start address
+        ; save old stack value
+        LD (ALASM_STACK),SP
+        LD SP,(PROGRAM_STACK)
+        ;save old registry values
+        PUSH AF
+        PUSH BC
+        PUSH DE
+
+        ; set output stream to screen
+        LD A,2      
+        CALL SCREEN_STREAM
+        
+        LD BC,#0A08
+        LD A,%00110100
+        CALL SET_SCREEN_ATTR
+
+        ; output Hello world string
+        ;LD DE,HELLO_STR
+        ;LD BC,12
+        ;CALL PRINT_STRING
+
+        ; return old registry values
+        POP DE
+        POP BC
+        POP AF
+        ; return old stack value
+        LD SP,(ALASM_STACK)
+        RET
+
+;SET SCREEN ATTRIBUTE FUNCTION
+;PARAMETERS:
+    ; BC - Y AND X COORDINATES
+    ; D  - ATTRIBUTES
+    ; 0..3 - Ink
+    ; 4..6 - Paper
+    ; 5    - Bright
+    ; 6    - Flash
+SET_SCREEN_ATTR:
+        PUSH AF     
+
+        LD A,22
+        RST 16
+        LD A,B
+        RST 16
+        LD A,C
+        RST 16
+
+        LD AF,0
+
+        ; Ink
+        LD A,D
+        AND %00000111
+                
+        PUSH AF
+        LD A,16
+        RST 16
+        POP AF
+        RST 16
+
+        ; Paper
+        LD A,D
+        AND %00111000
+        RLCA
+        RLCA
+        RLCA
+              
+        PUSH AF
+        LD A,17
+        RST 16
+        POP AF
+        RST 16 
+
+        POP AF
+
+        RET
+
+; GLOBAL VARIABLES AND DATA
+PROGRAM_STACK   DEFW #6000
+ALASM_STACK     DEFW #0000
+HELLO_STR       DEFB "Hello World!",0
