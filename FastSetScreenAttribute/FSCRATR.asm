8608520df58be02e43e8c44b5cc8fa9868d7c76c
izemskov <pascal.ilya@gmail.com>
2021-09-08 15:38:19 +0300

    Fast set screen attribute & fill screen function

---
+++ FastSetScreenAttribute/FSCRATR.asm
@@ -0,0 +1,162 @@
+;FAST SET SCREEN ATTRIBUTE
+;
+;ASM:     ALASM v5.07
+;CPU:     Zilog Z80, 3.5MHz
+;RAM:     48Kb or 128Kb
+;SCREEN:  265x192 pixels,
+;         32x24 color attributes
+;CTRL:    Keyboard and joystick
+;
+;AUTHOR:  Ilya Zemskov, 2021
+;         pascal.ilya@gmail.com
+
+SCREEN_STREAM    EQU 5633
+PRINT_STRING     EQU 8252
+SCREEN_ATTRIB    EQU #5800
+SCREEN_ATTRIB_H  EQU #58
+
+        ORG #6000   ; start address
+        ; save old stack value
+        LD (ALASM_STACK),SP
+        LD SP,(PROGRAM_STACK)
+        ;save old registry values
+        PUSH AF
+        PUSH BC
+        PUSH DE
+
+        ; set output stream to screen
+        LD A,2      
+        CALL SCREEN_STREAM
+
+        LD A,%00110100
+        CALL FILL_BACKGROUND
+
+        LD BC,#0505
+        LD A,%00101101
+        CALL SET_SCREEN_ATTR
+
+        ; return old registry values
+        POP DE
+        POP BC
+        POP AF
+        ; return old stack value
+        LD SP,(ALASM_STACK)
+        RET
+
+;SET SCREEN ATTRIBUTE FUNCTION
+;PARAMETERS:    
+    ; BC - Y [0..23] AND
+    ;      X [0..31]
+    ; A  - ATTRIBUTES
+    ; 0..3 - Ink
+    ; 4..6 - Paper
+    ; 5    - Bright
+    ; 6    - Flash
+; Working with video memory directly.
+; For this I need to calculate the 
+; offset from start address of video
+; memory attributes #5800
+; Offset calculate by formula:
+;
+; OFFSET = Y * 32 + X
+;
+; But the max Y is 23 and when I
+; mult 32 * 23 I will have 8-bit registry
+; overflow. For avoid this situation I
+; divide Y to two parts:
+;
+; 00|010|111
+;
+; and mult it separately
+SET_SCREEN_ATTR:        
+        PUSH BC
+        ; save attribute param (A)
+        PUSH AF
+        
+        ; process low part of Y
+        LD A,B
+        AND %00000111
+        ; for mult on 32 I
+        ; can use left shift 5 times
+        RLCA
+        RLCA
+        RLCA
+        RLCA
+        RLCA
+        ; add X to low part of Y
+        OR C
+        ; all right, just now I have
+        ; low part Y + all X in
+        ; C registry
+        LD C,A
+        
+        ; procee high part of Y
+        LD A,B
+        AND %00111000
+        RLCA
+        RLCA
+        RLCA
+        RLCA
+        RLCA
+        ; just now I have high part of Y
+        ; in accumulator registry
+        ;
+        ; for get right address in video
+        ; memory just add high part
+        ; attributes address (#58)
+        ; to high part of Y
+        OR SCREEN_ATTRIB_H
+        ; I have right address in BC
+        LD B,A
+        ; restore attribute param (A)        
+        POP AF
+        
+        ; write attribute param to
+        ; address which stored in BC
+        LD (BC),A
+
+        POP BC
+
+        RET
+
+;FILL BACKGROUND FUNCTION
+;PARAMETERS:    
+    ; A  - ATTRIBUTES
+    ; 0..3 - Ink
+    ; 4..6 - Paper
+    ; 5    - Bright
+    ; 6    - Flash
+FILL_BACKGROUND:
+        PUSH BC
+        PUSH DE
+
+        ; count of iterations
+        LD BC,#2018
+        ; current coordinates
+        LD DE,#0000
+LOOP2:  PUSH BC
+        PUSH DE
+
+LOOP1:  PUSH BC
+        LD B,D
+        LD C,E
+        CALL SET_SCREEN_ATTR
+        INC E
+        POP BC
+        DJNZ LOOP1
+
+        POP DE
+        INC D
+
+        POP BC
+        DEC C
+        JR NZ,LOOP2        
+
+        POP DE
+        POP BC
+
+        RET
+
+; GLOBAL VARIABLES AND DATA
+PROGRAM_STACK   DEFW #6000
+ALASM_STACK     DEFW #0000
